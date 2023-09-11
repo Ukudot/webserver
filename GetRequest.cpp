a7114938ef603fd7075939f23de03b6614b107f7
Giovanni Panico <giovannipanico314@gmail.com>
2023-09-11 14:23:34 +0200

    refactored GetRequest

--- GetRequest.cpp
+++ GetRequest.cpp
@@ -6,7 +6,7 @@
 /*   By: adi-stef <adi-stef@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 11:12:56 by gpanico           #+#    #+#             */
-/*   Updated: 2023/09/08 16:51:42 by gpanico          ###   ########.fr       */
+/*   Updated: 2023/09/11 14:13:30 by gpanico          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,24 +69,9 @@ void	GetRequest::createRes(TreeNode<t_node> *config) {
 	loc = this->findLocation(config);
 	if (loc->getName() != "")
 		tmpPath = tmpPath.substr(loc->getName().length());
-	{
-		std::string	cgiName;
-
-		cgiName = this->path.substr(this->path.rfind("/"));
-		for (std::vector<t_cgi>::iterator ite = loc->getData().cgis.begin(); ite != loc->getData().cgis.end(); ite++) {
-			if ((*ite).eName == cgiName) {
-				this->execCgi(loc, (*ite), loc->getData().cgiBin + tmpPath);
-				return ;
-			}
-		}
-	} // todo cgi
-	for (size_t i = 0; i < loc->getData().redirections.size(); i++) {
-		if (tmpPath == loc->getData().redirections[i].src) {
-			this->errorCode = loc->getData().redirections[i].type == 'r' ? 302 : 301;
-			this->response = this->generateError(config->getData().errPages, loc->getData().redirections[i].dst);
-			return ;
-		}
-	}
+	// cgis && reds
+	if (this->cgi(loc, tmpPath) || this->reds(config, loc, tmpPath))
+		return ;
 	if (loc->getData().root == "") {
 		this->errorCode = 404;
 		this->response = this->generateError();
@@ -105,56 +90,44 @@ void	GetRequest::createRes(TreeNode<t_node> *config) {
 	DEBUG(CYAN + "response created" + RESET);
 }
 
+bool	GetRequest::reds(TreeNode<t_node> *config, TreeNode<t_node> *loc, std::string tmpPath) {
+	for (size_t i = 0; i < loc->getData().redirections.size(); i++) {
+		if (tmpPath == loc->getData().redirections[i].src) {
+			this->errorCode = loc->getData().redirections[i].type == 'r' ? 302 : 301;
+			this->response = this->generateError(config->getData().errPages, loc->getData().redirections[i].dst);
+			return (true);
+		}
+	}
+	return (false);
+}
+
+bool	GetRequest::cgi(TreeNode<t_node> *loc, std::string tmpPath) {
+	std::string	cgiName;
+
+	cgiName = this->path.substr(this->path.rfind("/") + 1);
+	for (std::vector<t_cgi>::iterator ite = loc->getData().cgis.begin(); ite != loc->getData().cgis.end(); ite++) {
+		DEBUG(PURPLE + "cgiName: " + cgiName + RESET);
+		if ((*ite).eName == cgiName) {
+			DEBUG(RED + "cgi found" + RESET);
+			this->execCgi(loc, (*ite), loc->getData().cgiBin + tmpPath);
+			return (true);
+		}
+	}
+	DEBUG(RED + "cgi not found" + RESET);
+	return (false);
+}
+
 void	GetRequest::execCgi(TreeNode<t_node> *loc, t_cgi &cgi, std::string path) {
 	pid_t						pid;
-	char						**argv;
-	char						**envp;
-	std::vector<std::string>	splittedEnv;
-	size_t						i = 0;
 	int							fds[2];
 	char						buff[1];
 	std::stringstream			output;
 	int							wstatus;
-
+	long						time;
 
 	pipe(fds);
-	pid = fork();
-	if (!pid) {
-		dup2(fds[1], 1);
-		close(fds[0]);
-		{
-			splittedEnv = Utils::ft_split(this->env, "&");
-			envp = new char*[splittedEnv.size() + Server::envp.size() + 1];
-			for (; i < Server::envp.size(); i++)
-				envp[i] = Server::envp[i];
-			for (size_t j = 0; j < splittedEnv.size(); j++, i++)
-				envp[i] = strdup(splittedEnv[j].c_str());
-			envp[i] = NULL;
-		}
-		if (cgi.type == "exe") {
-			argv = new char*[2];
-			if (path.rfind("/") == NPOS)
-				argv[0] = strdup(path.c_str());
-			else
-				argv[0] = strdup(path.substr(path.rfind("/") + 1).c_str());
-			argv[1] = NULL;
-			execve(path.c_str(), argv, envp);
-			exit(10);
-		}
-		argv = new char*[3];
-		argv[0] = strdup(cgi.type.c_str());
-		argv[1] = strdup(path.c_str());
-		argv[2] = NULL;
-		if (cgi.type == "bash")
-			execve(BASH, argv, envp);
-		else if (cgi.type == "php")
-			execve(PHP, argv, envp);
-		else
-			execve(PYTHON, argv, envp);
-		exit(177);
-	}
-	close(fds[1]);
-	long	time = Utils::ft_gettime();
+	pid = this->launchCgi(fds, cgi, path);
+	time = Utils::ft_gettime();
 	while (Utils::ft_gettime() - time < CGI_TIMEOUT) {
 		waitpid(pid, &wstatus, WNOHANG);
 		if (WIFEXITED(wstatus)) {
@@ -177,4 +150,64 @@ void	GetRequest::execCgi(TreeNode<t_node> *loc, t_cgi &cgi, std::string path) {
 	else
 		this->errorCode = 500;
 	this->response = this->generateError(loc->getData().errPages);
+}
+
+char	**GetRequest::updateEnvp(void) {
+	char						**envp;
+	std::vector<std::string>	splittedEnv;
+	size_t						i = 0;
+
+	splittedEnv = Utils::ft_split(this->env, "&");
+	envp = new char*[splittedEnv.size() + Server::envp.size() + 1];
+	for (; i < Server::envp.size(); i++)
+		envp[i] = Server::envp[i];
+	for (size_t j = 0; j < splittedEnv.size(); j++, i++)
+		envp[i] = strdup(splittedEnv[j].c_str());
+	envp[i] = NULL;
+	return (envp);
+}
+
+void	GetRequest::deleteMat(void **mat) {
+	for (int i = 0; mat[i]; i++) {
+		free(mat[i]);
+	}
+	delete [] mat;
+}
+
+pid_t	GetRequest::launchCgi(int *fds, t_cgi &cgi, std::string path) {
+	pid_t						pid;
+	char						**argv;
+	char						**envp;
+
+	pid = fork();
+	if (!pid) {
+		dup2(fds[1], 1);
+		close(fds[0]);
+		envp = this->updateEnvp();
+		if (cgi.type == "exe") {
+			argv = new char*[2];
+			if (path.rfind("/") == NPOS)
+				argv[0] = strdup(path.c_str());
+			else
+				argv[0] = strdup(path.substr(path.rfind("/") + 1).c_str());
+			argv[1] = NULL;
+			execve(path.c_str(), argv, envp);
+			exit(10);
+		}
+		argv = new char*[3];
+		argv[0] = strdup(cgi.type.c_str());
+		argv[1] = strdup(path.c_str());
+		argv[2] = NULL;
+		if (cgi.type == "bash")
+			execve(BASH, argv, envp);
+		else if (cgi.type == "php")
+			execve(PHP, argv, envp);
+		else
+			execve(PYTHON, argv, envp);
+		this->deleteMat((void **) argv);
+		this->deleteMat((void **) envp);
+		exit(177);
+	}
+	close(fds[1]);
+	return (pid);
 }
--- GetRequest.hpp
+++ GetRequest.hpp
@@ -6,7 +6,7 @@
 /*   By: gpanico <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 11:12:56 by gpanico           #+#    #+#             */
-/*   Updated: 2023/09/08 16:50:46 by gpanico          ###   ########.fr       */
+/*   Updated: 2023/09/11 14:06:20 by gpanico          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,12 @@
 class	GetRequest: public ARequest {
 	private:
 
+		char	**updateEnvp(void);
+		bool	reds(TreeNode<t_node> *config, TreeNode<t_node> *loc, std::string tmpPath);
+		bool	cgi(TreeNode<t_node> *loc, std::string tmpPath);
 		void	execCgi(TreeNode<t_node> *loc, t_cgi &cgi, std::string path);
+		pid_t	launchCgi(int *fds, t_cgi &cgi, std::string path);
+		void	deleteMat(void **mat);
 
 	public:
 
